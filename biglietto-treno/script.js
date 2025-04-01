f84f182c692d84746d30c5d7296aa8a519cd3771
giuseppeesposito22 <gius.espo22@gmail.com>
2025-04-01 16:05:38 +0200

    aggiunto controllo sui dati inseriti

--- biglietto-treno/script.js
+++ biglietto-treno/script.js
@@ -14,6 +14,9 @@ const eta = parseInt(prompt("Quanti anni hai?"));
 
 console.log("numero chilometri " + numKm, "età " + eta);
 
+// variabile per verificare il corretto inserimento dei dati
+const datiCorretti = !isNaN(numKm) && !isNaN(eta);
+
 // calcolo il prezzo del biglietto in base ai km scelti
 const prezzoKm = 0.21;
 
@@ -29,31 +32,38 @@ console.log("sconto da applicare 40% " + discount40.toFixed(2));
 
 let prezzoFinale;
 
-// se l'età dell' utente è minore di 18 si applica il 20% di sconto
-if (eta <= 18) {
-  prezzoFinale = prezzoBiglietto - discount20;
-  console.log(
-    "prezzo finale con il 20% di sconto applicato " +
-      prezzoFinale.toFixed(2) +
-      "€"
-  );
-}
-// se l'età dell' utente è maggiore di 65 si applica il 40% di sconto
-else if (eta >= 65) {
-  prezzoFinale = prezzoBiglietto - discount40;
-  console.log(
-    "prezzo finale con il 40% di sconto applicato " +
-      prezzoFinale.toFixed(2) +
-      "€"
-  );
-}
-// se l'età dell' utente è compresa tra i 18 e i 65 non si applica nessuno sconto
-else {
-  prezzoFinale = prezzoBiglietto;
-  console.log(
-    "prezzo finale senza sconto applicato " + prezzoFinale.toFixed(2) + "€"
-  );
-}
+// se i dati inseriti sono corretti entra all'interno della condizione e calcola il prezzo del biglietto in base all'età
+if (datiCorretti) {
+  // se l'età dell' utente è minore di 18 si applica il 20% di sconto
+  if (eta < 18) {
+    prezzoFinale = prezzoBiglietto - discount20;
+    console.log(
+      "prezzo finale con il 20% di sconto applicato " +
+        prezzoFinale.toFixed(2) +
+        "€"
+    );
+  }
+  // se l'età dell' utente è maggiore di 65 si applica il 40% di sconto
+  else if (eta > 65) {
+    prezzoFinale = prezzoBiglietto - discount40;
+    console.log(
+      "prezzo finale con il 40% di sconto applicato " +
+        prezzoFinale.toFixed(2) +
+        "€"
+    );
+  }
+  // se l'età dell' utente è compresa tra i 18 e i 65 non si applica nessuno sconto
+  else {
+    prezzoFinale = prezzoBiglietto;
+    console.log(
+      "prezzo finale senza sconto applicato " + prezzoFinale.toFixed(2) + "€"
+    );
 
-// indico all'utente il prezzo da pagare
-alert("costo del biglietto " + prezzoFinale.toFixed(2) + "€");
+    // indico all'utente il prezzo da pagare
+    alert("costo del biglietto " + prezzoFinale.toFixed(2) + "€");
+  }
+}
+// se i dati non sono corretti segnala l'errore
+else {
+  alert("dati non corretti");
+}
